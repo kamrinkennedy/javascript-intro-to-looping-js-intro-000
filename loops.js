db83b930c951d63c44fc7a9d6630df801a2f04e4
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-11 01:57:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop (array) {
-  
+
   for (let i = 0, i < 25, i++) {
     if (i = 1) {
       array.push('I am 1 strange loop.');
@@ -7,4 +7,5 @@ function forLoop (array) {
       array.push(`I am #{i} strange loops.`)
     }
   }
+  return array
 }
